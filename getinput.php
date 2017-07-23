a2d74ed473787d1f77ba9bec0c2a589c0d083994
Skprose <sangeethakperumal@gmail.com>
2017-07-23 13:13:00 -0600

    first initial contact form

---
+++ getinput.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+    <p>Thanks for submitting your details. I will contact
+    <?php echo$_POST["name"]; ?><br> by <? echo$_POST["email"]; ?>
+    as soon as possible</p>
+
+
+</body>
+</html>
